af1bb854b778b90705a8f9dc17dd7984facf8f08
hannahilea <hannahilea@users.noreply.github.com>
2024-10-29 15:29:23 -0400

    add manually trace-compiled file to compilation

--- compile.jl
+++ compile.jl
@@ -3,4 +3,5 @@ Pkg.activate(@__DIR__)
 using PackageCompiler
 
 create_app("StallNotes", "StallNotesApp"; force=true,
+           precompile_statements_file="_precompile.jl",
            executables=["summarize" => "summarize_posts", "main" => "reflect_default"])
