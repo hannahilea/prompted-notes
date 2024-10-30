03e93bfe23caad339f7aa2537195c44ed6d75a28
Hannah Robertson <hannahilea@users.noreply.github.com>
2024-10-29 20:54:49 -0400

    Feat: Load non-default templates from input dir (#8)
    * format yaml
    
    * load template from directcory
    
    * shuffle
    
    * update readme
    
    * format

--- PromptedNotes.jl/src/PromptedNotes.jl
+++ PromptedNotes.jl/src/PromptedNotes.jl
@@ -1,6 +1,6 @@
 module PromptedNotes
 
-export load_template, prompt_template
+export load_template, prompt_from_template, prompt_from_templates
 
 using Dates
 using WordCloud
@@ -9,34 +9,36 @@ using OrderedCollections
 using YAML
 
 const DEFAULT_SAVE_DIR = expanduser(joinpath("~", "Desktop", "stall-notes"))
+const DEFAULT_TEMPLATE_DIR = joinpath(pkgdir(PromptedNotes), "..", "note-templates")
 
-# TODO-future: define Template as Legolas type; load accordingly; will remove many of the 
-# various safety-checks here 
-function load_template(template_path)
-    template = YAML.load_file(template_path; dicttype=OrderedDict{String,Any})
-    prefix = get(template, "prefix", "")
-    questions = let
-        q = get(template, "questions", [])
-        filter(x -> !isempty(get(x, "prompt", "")), q)
-    end
-    isempty(questions) &&
-        throw(ArgumentError("No `questions` found for template $(template_path)"))
+#####
+##### Prompting from templates 
+#####
 
-    if any(q -> isempty(get(q, "label", "")) || isempty(get(q, "prompt", "")), questions)
-        throw(ArgumentError("Questions must have non-empty labels and prompts: $(questions)"))
-    end
-
-    if length(questions) != length(Set([lowercase(get(q, "label", "")) for q in questions]))
-        throw(ArgumentError("Question labels must be unique (case invariant): $(questions)"))
-    end
-
-    opening = get(template, "opening-comment", "Hello, note-taker!")
-    closing = get(template, "closing-comment", "Thank you!")
-    return (; prefix, questions, opening, closing)
+function get_single_template(template_dir)
+    paths = get_template_paths(template_dir)
+    return length(paths) == 1 ? first(paths) : prompt_select_template(paths)
 end
 
-# Note: not doing any yaml validation; if the template file is set up wrong, oh well, game over!
-function prompt_template(dir; template_path)
+function get_template_paths(template_dir)
+    paths = filter(p -> endswith(p, ".template.yaml") || endswith(p, ".template.yml"),
+                   readdir(template_dir; join=true))
+    if isempty(paths)
+        paths = filter(p -> endswith(p, ".template.yaml") || endswith(p, ".template.yml"),
+                       readdir(DEFAULT_TEMPLATE_DIR; join=true))
+    end
+    return paths
+end
+
+#TODO-do this for real 
+prompt_select_template(template_paths) = first(sort(template_paths))
+
+function prompt_from_templates(; out_dir=DEFAULT_SAVE_DIR, template_dir=out_dir)
+    template_path = get_single_template(template_dir)
+    return prompt_from_template(; out_dir, template_path)
+end
+
+function prompt_from_template(; out_dir, template_path)
     # Get input template 
     (; prefix, questions, opening, closing) = load_template(template_path)
 
@@ -46,7 +48,7 @@ function prompt_template(dir; template_path)
 
     # Set up output file
     date = now()
-    filepath = joinpath(dir, "$prefix-$date.yaml")
+    filepath = joinpath(out_dir, "$prefix-$date.yaml")
     while isfile(filepath)
         # Avoid overwriting an existing timestamp
         # Could still run into race condition BUT that is very unlikely 
@@ -80,6 +82,31 @@ function prompt_template(dir; template_path)
     return nothing
 end
 
+# TODO-future: define Template as Legolas type; load accordingly; will remove many of the 
+# various safety-checks here 
+function load_template(template_path)
+    template = YAML.load_file(template_path; dicttype=OrderedDict{String,Any})
+    prefix = get(template, "prefix", "")
+    questions = let
+        q = get(template, "questions", [])
+        filter(x -> !isempty(get(x, "prompt", "")), q)
+    end
+    isempty(questions) &&
+        throw(ArgumentError("No `questions` found for template $(template_path)"))
+
+    if any(q -> isempty(get(q, "label", "")) || isempty(get(q, "prompt", "")), questions)
+        throw(ArgumentError("Questions must have non-empty labels and prompts: $(questions)"))
+    end
+
+    if length(questions) != length(Set([lowercase(get(q, "label", "")) for q in questions]))
+        throw(ArgumentError("Question labels must be unique (case invariant): $(questions)"))
+    end
+
+    opening = get(template, "opening-comment", "Hello, note-taker!")
+    closing = get(template, "closing-comment", "Thank you!")
+    return (; prefix, questions, opening, closing)
+end
+
 function replace_braces(str, dict)
     contains(str, "{{ ") || return str
     for k in keys(dict)
@@ -88,26 +115,9 @@ function replace_braces(str, dict)
     return str
 end
 
-function prompt_reflection(; dir=DEFAULT_SAVE_DIR)
-    prompt_template(dir;
-                    template_path=joinpath(pkgdir(PromptedNotes), "templates",
-                                           "default-stall-template.yaml"))
-    return nothing
-end
-
-function wordcloud_from_posts(paths::AbstractVector{<:AbstractString})
-    lines = map(paths) do p
-        answers = YAML.load_file(p; dicttype=OrderedDict)
-        return join([v for v in values(answers)], "\n")
-    end
-    words = join(lines, "\n")
-
-    # See https://github.com/guo-yong-zhi/WordCloud-Gallery/blob/main/README.md 
-    # for styling options
-    wc = paintcloud(words)
-    display(wc)
-    return wc
-end
+#####
+##### Note summaries
+#####
 
 function summarize(dir::AbstractString)
     println("Analyzing posts in $dir...")
@@ -126,8 +136,26 @@ function summarize(dir::AbstractString)
     return nothing
 end
 
+function wordcloud_from_posts(paths::AbstractVector{<:AbstractString})
+    lines = map(paths) do p
+        answers = YAML.load_file(p; dicttype=OrderedDict)
+        return join([v for v in values(answers)], "\n")
+    end
+    words = join(lines, "\n")
+
+    # See https://github.com/guo-yong-zhi/WordCloud-Gallery/blob/main/README.md 
+    # for styling options
+    wc = paintcloud(words)
+    display(wc)
+    return wc
+end
+
+#####
+##### App entrypoints
+#####
+
 function reflect_default()::Cint
-    prompt_reflection(; dir=DEFAULT_SAVE_DIR)
+    prompt_reflection(; dir=DEFAULT_SAVE_DIR, template_dir=DEFAULT_TEMPLATE_DIR)
     println("(Hit `Enter` to close)")
     readline()
     return 0 # if things finished successfully
--- PromptedNotes.jl/test/runtests.jl
+++ PromptedNotes.jl/test/runtests.jl
@@ -2,14 +2,31 @@ using Test
 using PromptedNotes
 using Aqua
 
+const TESTCASE_DIR = joinpath(pkgdir(PromptedNotes), "test", "testcases")
+
 @testset "PromptedNotes.jl" begin
     @testset "Aqua" begin
         Aqua.test_all(PromptedNotes; ambiguities=false)
     end
 
-    @testset "Basic" begin
-        template_path = joinpath(pkgdir(PromptedNotes), "templates",
-                                 "default-stall-template.yaml")
+    @testset "template paths" begin
+        using PromptedNotes: get_template_paths, get_single_template,
+                             DEFAULT_TEMPLATE_DIR
+        @test length(get_template_paths(joinpath(TESTCASE_DIR, "dir-double-template"))) == 2
+        @test isequal(get_template_paths(mktempdir()),
+                      get_template_paths(DEFAULT_TEMPLATE_DIR))
+
+        @test isequal(get_single_template(mktempdir()),
+                      get_single_template(DEFAULT_TEMPLATE_DIR))
+
+        dir = joinpath(TESTCASE_DIR, "dir-single-template")
+        @test isequal(get_single_template(dir),
+                      joinpath(dir, "ex.template.yaml"))
+    end
+
+    @testset "template loading" begin
+        template_path = joinpath(PromptedNotes.DEFAULT_TEMPLATE_DIR,
+                                 "default-stall.template.yaml")
         t = load_template(template_path)
         @test length(t.questions) == 6
         @test issetequal(keys(t), [:prefix, :questions, :opening, :closing])
